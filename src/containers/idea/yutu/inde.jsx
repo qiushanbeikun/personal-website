164700ba80820fe68d9f87256b0c7f91da544770
qiushanbeikun <beikuncanada@gmail.com>
2020-01-03 19:17:34 -0800

    change background image

---
+++ src/containers/idea/yutu/inde.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default function YUTUMainPage() {
+  return (
+    <div>
+      <Typography variant={"h3"}>YUTU</Typography>
+    </div>
+  )
+}
