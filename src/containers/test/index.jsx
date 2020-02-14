b45729ec246167e0a9fe1cc3daf64da94aa85cd9
qiushanbeikun <beikuncanada@gmail.com>
2020-02-13 17:39:11 -0800

    finish the search algorithm

--- src/containers/test/index.jsx
+++ src/containers/test/index.jsx
@@ -1,12 +1,9 @@
 import React, {useState} from "react";
-import Container from "@material-ui/core/Container";
-import CommentBox from "../../components/CommentSystem/CommentBox";
 import Button from "@material-ui/core/Button";
 
 export default function TestPage() {
 
-  const existingComments =
-    [
+  const existingComments = [
       {
         name: "testestest",
         email: "test1@gmail.com",
@@ -66,29 +63,67 @@ export default function TestPage() {
         reply: []
       }];
 
-  const [countOfSolutions, setCountOfSolutions] = React.useState(0);
+  let countOfSolutions = 0;
+  let solutions = [];
+  let countOfFailingBranches = 0;
 
-  const [countOfFailingBranches, setCountOfFailingBranches] = React.useState(0);
-
-  const [variables, setVariables] = React.useState([]);
-
-  const testSetVariables = () => {
-    setVariables([...variables,1,2]);
-    console.log(variables);
-  };
+  let variables = [];
 
   const constraintTest = () => {
-    console.log(true);
-    assignOrder(true);
+
+    let constraint1 = (isNaN(variables[6]))? true : (variables[0] >= variables[6]);
+    let constraint2 = (isNaN(variables[7]))? true : (variables[0] < variables[7]);
+    let constraint3 = (isNaN(variables[5]))? true : (variables[1] - variables[5] === 1 || variables[1] - variables[5] === -1 );
+    let constraint4 = (isNaN(variables[7]))? true : (variables[6] < variables[7]);
+
+    let constraint5 = (isNaN(variables[6]))? true : (variables[6] -variables[2] === 1 ||variables[6]-variables[2] === -1);
+    let constraint6 = (isNaN(variables[7]))? true : ((variables[7] - variables[2]) % 2 === 0);
+    let constraint7 = (isNaN(variables[7]))? true : (variables[7] !== variables[3]);
+    let constraint8 = (isNaN(variables[6]))? true : (variables[3] >= variables[6]);
+
+    let constraint9 = (isNaN(variables[3])) ? true : (variables[3] !== variables[2]);
+    let constraint10 = (isNaN(variables[4]))? true : (variables[4] !== variables[2]);
+    let constraint11 = (isNaN(variables[4]))? true : (variables[4] < variables[3] -1);
+    let constraint12 = (isNaN(variables[4]))? true : (variables[4] !== variables[7] -2);
+
+    let constraint13 = (isNaN(variables[6]))? true : (variables[6] !== variables[5]);
+    let constraint14 = (isNaN(variables[7]))? true : (variables[7] !== variables[5]);
+    let constraint15 = (isNaN(variables[5]))? true : (variables[2] !== variables[6]);
+    let constraint16 = (isNaN(variables[5]))? true : (variables[3] !== variables[5]);
+
+    let constraint17 = (isNaN(variables[5]))? true : ((variables[5] - variables[4]) % 2 === 1 || (variables[5] - variables[4]) % 2 === -1);
+
+    const testResult = (
+
+      constraint1 &&
+      constraint2 &&
+      constraint3 &&
+      constraint4 &&
+
+      constraint5 &&
+      constraint6 &&
+      constraint7 &&
+      constraint8 &&
+
+      constraint9 &&
+      constraint10 &&
+      constraint11 &&
+      constraint12 &&
+
+      constraint13 &&
+      constraint14 &&
+      constraint15 &&
+      constraint16 &&
+
+      constraint17
+    );
+    console.log("current variables:" + variables);
+    console.log(testResult);
+    (testResult)? countOfFailingBranches+= 0 : countOfFailingBranches += 1;
+    assignOrder(testResult);
   };
 
-  const assignOrder = (previousTest, event) => {
-
-    console.log("current array: " + variables);
-    console.log(variables.length);
-    // const currentIndex = variables.length;
-    const lastDigit = variables[variables.length-1];
-
+  const assignOrder = (previousTest) => {
 
     // if test is true
     //    if array.length < 8
@@ -112,64 +147,58 @@ export default function TestPage() {
 
     if (previousTest){
       if (variables.length < 8) {
-        console.log("@@@@@branch1");
-        setVariables([...variables, 1]);
-        debugger;
+        variables = [...variables, 1];
         constraintTest();
 
       } else { // currentIndex = 8
-        setCountOfSolutions(countOfSolutions+1);
+        countOfSolutions += 1;
+        solutions += variables;
+        if (variables[variables.length-1] !== 4 && !isNaN(variables[variables.length-1])) {
+          variables = [...variables.splice(0,variables.length-1), variables[variables.length-1]+1];
+          constraintTest();
+        } else {
+          while (variables[variables.length-1] === 4){
+            variables = variables.splice(0, variables.length-1);
+          }
+          if (variables.length > 0) {
+            variables = [
+              ...(variables.splice(0,variables.length-1)),
+              variables[variables.length-1]+1];
+            constraintTest();
+          }
+          else {
+            console.log("End of search");
+          }
+        }
+      }
+    } else {
+      // debugger;
+      if (variables[variables.length-1] !== 4 && !isNaN(variables[variables.length-1])) {
+        variables = [...variables.splice(0,variables.length-1), variables[variables.length-1]+1];
+        constraintTest();
+      } else {
         while (variables[variables.length-1] === 4){
-          setVariables(variables.splice(0, variables.length-2));
+          variables = variables.splice(0, variables.length-1);
         }
         if (variables.length > 0) {
-          setVariables([
-            ...(variables.splice(0,variables.length-3)),
-            variables[variables.length-2]+1]);
+          variables = [
+            ...(variables.splice(0,variables.length-1)),
+            variables[variables.length-1]+1];
           constraintTest();
         }
         else {
           console.log("End of search");
+          console.log("Number of solutions: " + countOfSolutions);
+          console.log("Number of Failing branches: " + countOfFailingBranches);
+          console.log(solutions);
         }
       }
-    } else {
-      while (variables[variables.length-1] === 4){
-        setVariables(variables.splice(0, variables.length-2));
-      }
-      if (variables.length > 0) {
-        setVariables([
-          ...(variables.splice(0,variables.length-3)),
-          variables[variables.length-2]+1]);
-        constraintTest();
-      }
-      else {
-        console.log("End of search");
-      }
     }
-
-
-
-
-    // (lastDigit != 4) increment last digit : move to previous digit and repeat
   };
 
   const handleClick = (event) => {
     event.preventDefault();
-
-    // console.log(countOfFailingBranches);
-    // console.log(countOfSolutions);
-
-
     assignOrder(true);
-
-
-
-
-    const domains = [1, 2, 3, 4];
-    const constraints = [];
-    variables.map((eachVariables, i ) => (
-      console.log()
-    ))
   };
 
   return (
